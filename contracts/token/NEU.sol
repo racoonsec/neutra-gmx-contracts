1f4a0d90027f2043ef2dd39cc691d11538c6705a
racoonsec <wnduplessie@gmail.com>
2024-09-16 16:30:41 -0400

    Update RNX.sol

--- contracts/token/NEU.sol
+++ contracts/token/NEU.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.11;
 
 import {MintableERC20} from "./base/MintableERC20.sol";
 
-contract NEU is MintableERC20 {
-    constructor() MintableERC20("Neutra Token", "NEU") {}
+contract RNX is MintableERC20 {
+    constructor() MintableERC20("Racconswap Exhange Token", "RNX") {}
 
     // no need to correspond with want decimals
     function decimals() public pure override returns (uint8) {
